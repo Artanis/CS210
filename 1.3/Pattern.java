481700bd4989c4028cb4b1984c7188e04c7aa5fb
erik <erik@erik-netbook.(none)>
2009-08-31 20:07:25 -0700

    Some more comments on assignment 1.3

--- 1.3/Pattern.java
+++ 1.3/Pattern.java
@@ -1,15 +1,20 @@
 public class Pattern {
+	/**
+	 * Main Application
+	 *
+	 */
 	public static void main(String[] args) {
 		System.out.println(Pattern.pattern());
 	}
 	
+	/**
+	 * Returns a string containing the text JAVA in a large pattern
+	 * 
+	 * TODO: Render arbitrary strings passed through an argument. 
+	 * 
+	 * @return String pattern
+	 */
 	public static String pattern() {
-		/* Returns a string containing the text JAVA in a large pattern
-		
-		TODO: Render arbitrary strings passed through an argument.
-		
-		*/
-		
 		String pattern =
 		"     J     A    V     V    A    \n" +
 		"     J    A A    V   V    A A   \n" +
