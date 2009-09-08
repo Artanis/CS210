fefa11dad98359561ca752fe994d15aaf1ab1a1c
Erik Youngren <artanis.00@gmail.com>
2009-09-08 13:18:01 -0700

    Impulsive commenting of python script (good habit)

--- 6.25/matrix.py
+++ 6.25/matrix.py
@@ -1,7 +1,59 @@
-def get_rows (a):
-	return len(a)
+""" Multiply two matrices together
 
-def get_cols (a):
+Under normal circumstances I'd have made a matrix type and written the
+multiply in under the __mul__ magic class method, enabling the use of
+the multiplicative operator in multiplying two matrices, but since this
+is just a proof of concept for a Java assignment (and I have no
+intention of delving into type creation in Java at this point in time,
+this is just left as a simple script.
+
+Hard-coded parameters:
+    Matrix a:
+        [1,2]
+        [3,4]
+    Matrix b:
+        [1,1]
+        [1,1]
+
+Result:
+    Matrix c:
+        [3,3]
+        [7,7]
+
+"""
+
+def get_rows (matrix):
+	""" Returns the number of rows in a matrix
+	
+	Really just the length of a list, a wrapper for
+	
+	    len(a)
+	
+	Here for completeness, paired with the get_cols function
+	
+	Named Arguments:
+	* matrix -- any list, tuple, or dict object
+	
+	Returns number of elements in matrix
+	
+	"""
+	
+	return len(matrix)
+
+def get_cols (matrix):
+	""" Returns the number of
+	
+	Counts length of each row in matrix. If the matrix is ragged,
+	returns -1. If the matrix is rectangular returns the number of
+	columns.
+	
+	Named Arguments:
+	* matrix -- any list of lists, tuple of tuples, or dict of dicts.
+	
+	Returns number of columns in matrix, or -1 if matrix is ragged
+	
+	"""
+	
 	result = None
 	for i in a:
 		c = len(i)
