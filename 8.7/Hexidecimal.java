0562c5e54a4a26cf51578e6487e569b4f6ae7727
Erik Youngren <artanis.00@gmail.com>
2009-09-09 00:50:14 -0700

    Assignment 8.7 - Complete

--- 8.7/Hexidecimal.java
+++ 8.7/Hexidecimal.java
@@ -2,13 +2,12 @@ import javax.swing.JOptionPane;
 
 public class Hexidecimal {
 	public static void main (String[] args) {
-		String valid = "0123456789ABCDEFGabcdefg";
+		String hex = Hexidecimal.get_hexstring();
 		
-		for (int i = 0; i < valid.length(); i++) {
-			System.out.println(String.format(
-				"%s %d %b", valid.charAt(i), (int) valid.charAt(i),
-				Hexidecimal.valid_digit(valid.charAt(i))));
-		}
+		int decimal = Hexidecimal.parseHex(hex);
+		
+		JOptionPane.showMessageDialog(null, String.format(
+			"0x%s is decimal %d", hex, decimal));
 	}
 	
 	/**
@@ -17,7 +16,27 @@ public class Hexidecimal {
 	 * @return int
 	 */
 	public static int parseHex(String hex) {
-		return 0;
+		int magnitude = hex.length()-1;
+		
+		int decimal = 0;
+		
+		for (int i = magnitude; i >= 0; i--) {
+			int digit = Hexidecimal.convert_digit(hex.charAt(i));
+			int place = magnitude-i;
+			
+			//System.out.println(String.format("%s=%d",hex.charAt(i),
+				//digit));
+			//System.out.println(String.format("  digit = %d * 16^%d",
+			//	digit, place));
+			//System.out.println(String.format("  digit = %d * %d",
+			//	digit, (int)Math.pow(16,place)));
+			//System.out.println(String.format("  digit = %d",
+			//	(int)(digit * Math.pow(16,place))));
+			
+			decimal += (int)(digit * Math.pow(16, place));
+		}
+		
+		return decimal;
 	}
 	
 	/**
@@ -26,7 +45,47 @@ public class Hexidecimal {
 	 * @return String
 	 */
 	public static String get_hexstring() {
+		String hex_string = JOptionPane.showInputDialog(null,
+			"Enter a hexidecimal integer", "Hexidecimal -> Decimal",
+			JOptionPane.QUESTION_MESSAGE);
+		
+		if(Hexidecimal.validate(hex_string)) {
+			return hex_string;
+		} else {
+			JOptionPane.showMessageDialog(null, String.format(
+				"Invalid hexidecimal integer \"0x%s\"", hex_string));
+		}
+		
 		return new String();
+	}
+	
+	public static int convert_digit(char hex) {
+		int value = 0;
+		if (hex <= '9') {
+			value = hex - '0';
+		} else if(hex <= 'F') {
+			value = hex - 'A' + 10;
+		} else if(hex <= 'f') {
+			value = hex - 'a' + 10;
+		}
+		return value;
+	}
+	
+	/**
+	 * Checks that the hex string is valid
+	 *
+	 * @param String hex
+	 * 
+	 * @return boolean
+	 */
+	public static boolean validate(String hex) {
+		for (int i = 0; i < hex.length(); i++) {
+			if(!Hexidecimal.valid_digit(hex.charAt(i))) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 	
 	/**
