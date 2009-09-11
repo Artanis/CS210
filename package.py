ac79b2299923deab980b61419a9e1e98028296cf
Erik Youngren <artanis.00@gmail.com>
2009-09-10 23:01:55 -0700

    Quick script to package up an assignment folder

---
+++ package.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import sys
+import zipfile
+from glob import glob
+
+def package_assignment(name):
+	""" Creates a zip file containing the assignment ready to turn in
+	
+	Packages all .java files as well as README
+	
+	"""
+	
+	assignment = name.replace(".","-")
+	arcname = "[Erik_Youngren]_Assignment_%s" % (assignment,)
+	
+	package = zipfile.ZipFile("%s.zip" % (arcname,), "a")
+	
+	filelist = glob(name+"/*.java")
+	filelist.extend(glob(name+"/README"))
+	
+	print "Creating %s.zip" % (arcname,)
+	for f in filelist:
+		arcfile = "%s%s" % (arcname, f[f.find("/"):])
+		package.write(f, arcfile)
+		print "    %s" % (arcfile,)
+
+if __name__ == "__main__":
+	package_assignment(sys.argv[1])
