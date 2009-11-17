50868b4d2f1f39570e1e4a4bfb1885b35d1a990c
Erik Youngren <artanis.00@gmail.com>
2009-11-16 19:34:59 -0800

    Replaced space in a variable name with underscore

--- Exam03/CountRandom.java
+++ Exam03/CountRandom.java
@@ -16,12 +16,22 @@ public class CountRandom {
         
         int max = CountRandom.max(counts);
         
-        
         for (int i = 0; i < counts.length; i++) {
-            System.out.print(CountRandom.print(i, counts[i], max));
+            System.out.print(CountRandom.toString(i, counts[i], max));
         }
     }
     
+    /**
+     * Makes the specified number of rolls, within the given range,
+     * and returns the number of hits each integer received.
+     * 
+     * @param int lower The lower bound for the roll
+     * @param int upper The upper bound for the roll
+     * @param int rolls The number of rolls to make
+     * 
+     * @return int[] Series of hits for each number in the range.
+     * 
+     */
     static int[] roll (int lower, int upper, int rolls) {
         int[] counts = new int[upper - lower + 1];
         
@@ -32,6 +42,12 @@ public class CountRandom {
         return counts;
     }
     
+    /**
+     * Find the maximum value in the series.
+     * 
+     * @return int maximum value
+     * 
+     */
     static int max (int[] array) {
         int max = 0;
         
@@ -44,14 +60,45 @@ public class CountRandom {
         return max;
     }
     
+    /**
+     * Creates a string displaying the number of hits each number got.
+     * 
+     * If the optional parameter high is provided, also creates a
+     * graphical representation.
+     * 
+     * @param int entry The number
+     * @param int quantity The number of hits it got
+     * @param int high Optional. The largest number of hits in the
+     *                 series.
+     * 
+     * @return String
+     * 
+     */
     static String print (int entry, int quantity) {
         return String.format("%3d %3d\n",
             entry, quantity);
     }
     
-    static String print (int entry, int quantity, int high) {
+    /**
+     * Creates a string displaying the number of hits each number got.
+     * 
+     * If the optional parameter high is provided, also creates a
+     * graphical representation.
+     * 
+     * @param int entry The number
+     * @param int quantity The number of hits it got
+     * @param int high Optional. The largest number of hits in the
+     *                 series.
+     * 
+     * @return String
+     * 
+     */
+    static String toString (int entry, int quantity, int high) {
+        final int SCREEN_WIDTH = 79 - 11;
+        
         String graph = "";
-        int size = (int)((double)quantity / high * 68);
+        
+        int size = (int)((double)quantity / high * SCREEN_WIDTH);
         for (int i = 0; i < size-1; i++) {
             graph += "-";
         }
@@ -65,10 +112,28 @@ public class CountRandom {
 }
 
 class Random {
+    /**
+     * Generate a rnadom number within the given range
+     * 
+     * @param int lower Optional. Default 0
+     * @param int upper
+     * 
+     * @return int
+     * 
+     */
     static int range (int upper) {
         return Random.range(0, upper);
     }
     
+    /**
+     * Generate a rnadom number within the given range
+     * 
+     * @param int lower Optional. Default 0
+     * @param int upper
+     * 
+     * @return int
+     * 
+     */
     static int range (int lower, int upper) {
         return (int)((Math.random() * (upper - lower + 1)) + lower);
     }
